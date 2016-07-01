06be10f7590b05e5371352347aa4ad639665465c
Peccansy <gotodno@gmail.com>
2016-07-01 15:28:56 +0300

    JS + media fixes

---
+++ js/main.js
@@ -0,0 +1,73 @@
+$( document ).ready( function() {
+
+    'use strict';
+    var count = 0;    
+    // Responsive navigation
+    // --------------------------------------------------
+    if ($(window).width()<=675) {
+
+    $( '.navbar-nav-toggle' ).on( 'click', function() { 
+
+    		if($('.navbar-nav-toggle').hasClass('open') & $('.row').css('position') == 'fixed' ){
+    				
+    					$('.row').addClass('mobile-navbar');    					 				 
+    			}
+
+
+    		if($('.navbar-nav-toggle').hasClass('open') & $(window).scrollTop()<=$('.row').height() ){
+    				
+    					$('.row').removeClass('mobile-navbar');	   				 
+    			}   	   		
+
+        	if($('.navbar-nav-toggle').hasClass('open')) {        		
+        	
+            	$( 'header nav ul' ).slideUp(function() {
+
+            		$('.navbar-nav-toggle').removeClass('open');
+            		$( 'header nav ul' ).removeClass( 'nav-open' );	
+            		
+
+            	} );
+
+            	
+            } else {
+          
+            		$( 'header nav ul' ).slideDown();
+            		$('.row').addClass('mobile-navbar');
+            		$( 'header nav ul' ).addClass( 'nav-open' );
+            		$('.navbar-nav-toggle').addClass('open');
+
+            };
+            
+        
+    } );    
+    // Scroll  function
+    //------------------------------------------------- 
+    $(window).scroll(function(){
+
+
+    	if ( $(window).scrollTop()>$('.row').height() ) {
+    		
+    		if (count==0) {
+    			$('.row').fadeOut(function(){    				
+    				$('.row a img').addClass('logo-sm');    				  		   		
+    				$('header .row').css('position','fixed');
+    				$('header .row').addClass('mobile-navbar');    		
+    			} );
+    		};
+    		$('.row').fadeIn('fast');    		
+    		count = 1;
+    		return (count);
+    		   
+    	} else {
+    		$('header .row').css('position','inherit');
+    		if(!$('.navbar-nav-toggle').hasClass('open')) {
+    			$('header .row').removeClass('mobile-navbar');	
+    		}    		
+    		$('.row a img').removeClass('logo-sm'); 
+    		count = 0;
+    		return(count);
+    	}
+
+    } );
+}
